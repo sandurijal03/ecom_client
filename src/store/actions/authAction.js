8f11e59975be4f5b88db15e577c5444a480935c4
Sandip Rijal <sandurijal03@hotmail.com>
2022-01-05 22:22:40 +0545

    user registration client side server logic in store

--- src/store/actions/authAction.js
+++ src/store/actions/authAction.js
@@ -1,5 +1,24 @@
-import { SET_CURRENT_USER } from '../types';
+import { SET_CURRENT_USER, SUCCESSFUL_REGISTER } from '../types';
 
 export const setCurrentUser = (user) => {
   return { type: SET_CURRENT_USER, payload: user };
+};
+
+export const register = (userData) => async (dispatch) => {
+  try {
+    const response = await fetch('http://localhost:8848/api/users/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userData),
+    });
+    const result = await response.json();
+    dispatch({
+      type: SUCCESSFUL_REGISTER,
+      payload: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,18 +1,34 @@
-import { SET_CURRENT_USER } from '../types';
-import { isEmpty } from 'lodash';
+import { ERRORS, SET_CURRENT_USER, SUCCESSFUL_REGISTER } from '../types';
+import { isEmpty, min } from 'lodash';
 
 const initialState = {
   isAuthenticated: false,
+  token: localStorage.getItem('token'),
   user: {},
 };
 
 export default function authReducer(state = initialState, action) {
-  switch (action.type) {
+  const { type, payload } = action;
+  switch (type) {
     case SET_CURRENT_USER:
       return {
         ...state,
-        isAuthenticated: !isEmpty(action.payload),
-        user: action.payload,
+        isAuthenticated: !isEmpty(payload),
+        user: payload,
+      };
+    case SUCCESSFUL_REGISTER:
+      localStorage.setItem('token', payload.token);
+      return {
+        ...state,
+        ...payload,
+        isAuthenticated: true,
+      };
+    case ERRORS:
+      localStorage.removeItem('token');
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
       };
     default:
       return state;
