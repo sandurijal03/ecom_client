e86da2c7ec193766bfcb5a16c07d12edbdd954b7
Sandip Rijal <sandurijal03@hotmail.com>
2022-01-06 23:42:12 +0545

    react redux to connect client to store

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import { StrictMode } from 'react';
 import { render } from 'react-dom';
 import Main from './Main';
 
+import { Provider } from 'react-redux';
+import store from './store/store';
+
 const root = document.getElementById('root');
 render(
   <StrictMode>
-    <Main />
+    <Provider store={store}>
+      <Main />
+    </Provider>
   </StrictMode>,
   root,
 );
